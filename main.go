72391153e72005aaf38b58c84251c6d7d190215e
henryppercy <henryppercy@gmail.com>
2025-11-18 16:26:32 +0000

    feat: added read file and print file

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"fmt"
+)
 
 func main () {
-	fmt.Println("Hello, Counter!")
+	data, _ := os.ReadFile("./words.txt")
+
+	fmt.Println("data:", string(data)) // is a slight performance impact when converting between string and bytes (vica verca), don't go switching back and forth, just as needed
 }
