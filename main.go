bc2e339d92ddddec0cfa3dc6642dac6e8828ab24
henryppercy <henryppercy@gmail.com>
2025-11-19 20:01:22 +0000

    feat: create functionality to count words from a file, reading a file from a buffer

--- main.go
+++ main.go
@@ -3,15 +3,51 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"os"
+	"unicode"
 )
 
 func main() {
-	data, _ := os.ReadFile("./words.txt")
+	filename := "./words.txt"
 
-	wordCount := CountWords(data)
+	log.SetFlags(0)
 
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln("failed to read file:", err)
+	}
+
+	wordCount := CountWordsInFile(file)
 	fmt.Println(wordCount)
+}
+
+func CountWordsInFile(file *os.File) int {
+	wordCount := 0
+	isInsideWord := false
+
+	const bufferSize = 3
+	buffer := make([]byte, bufferSize)
+
+	for {
+		size, err := file.Read(buffer)
+		if err != nil {
+			break
+		}
+
+		isInsideWord = !unicode.IsSpace(rune(buffer[0])) && isInsideWord 
+
+		bufferCount := CountWords(buffer[:size])
+		if isInsideWord {
+			bufferCount -= 1
+		}
+
+		wordCount += bufferCount
+
+		isInsideWord = !unicode.IsSpace(rune(buffer[size-1]));
+	}
+
+	return wordCount
 }
 
 func CountWords(data []byte) int {
