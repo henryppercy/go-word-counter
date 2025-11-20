4880fb461703bf3634c8711ac4931e5e588516f8
henryppercy <henryppercy@gmail.com>
2025-11-20 18:21:31 +0000

    feat: refactor main function to accept filenames as arguments, the word count for each is printed along side total for all files

--- main.go
+++ main.go
@@ -9,17 +9,34 @@ import (
 )
 
 func main() {
-	filename := "./words.txt"
-
 	log.SetFlags(0)
 
+	if len(os.Args) < 2 {
+		log.Fatalln("error: must provide at least one file name")
+	}
+
+	total := 0
+
+	fileNames := os.Args[1:]
+	for _, f := range fileNames {
+		wordCount := CountWordsInFile(f)
+
+		total += wordCount
+		
+		fmt.Println(f, wordCount)
+	}
+
+	if len(fileNames) > 1 {
+		fmt.Println("total", total)
+	}
+}
+
+func CountWordsInFile(filename string) int {
 	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatalln("failed to read file:", err)
 	}
-
-	wordCount := CountWords(file)
-	fmt.Println(wordCount)
+	return CountWords(file)
 }
 
 func CountWords(file io.Reader) int {
