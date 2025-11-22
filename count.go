93b1af66a6d107125be059af099b2039b60fa03b
henryppercy <henryppercy@gmail.com>
2025-11-22 16:19:33 +0000

    feat: move counting code to new file

---
+++ count.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"os"
+)
+
+func CountWordsInFile(filename string) (int, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	count := CountWords(file)
+
+	return count, nil
+}
+
+func CountWords(file io.Reader) int {
+	wordCount := 0
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		wordCount++
+	}
+
+	return wordCount
+}
