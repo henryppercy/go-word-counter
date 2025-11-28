545a9df56c6cbd6d40875ce08a0f6066cd71d847
henryppercy <henryppercy@gmail.com>
2025-11-28 19:06:38 +0000

    feat: restructure repo

---
+++ counter/count.go
@@ -0,0 +1,132 @@
+package counter
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
+
+	"github.com/henryppercy/counter/display"
+)
+
+type Counts struct {
+	Bytes int
+	Words int
+	Lines int
+}
+
+func (c Counts) Add(other Counts) Counts {
+	c.Lines += other.Lines
+	c.Words += other.Words
+	c.Bytes += other.Bytes
+	return c
+}
+
+func (c Counts) Print(w io.Writer, opts display.Options, suffixes ...string) {
+	stats := []string{}
+
+	if opts.ShouldShowLines() {
+		stats = append(stats, strconv.Itoa(c.Lines))
+	}
+
+	if opts.ShouldShowWords() {
+		stats = append(stats, strconv.Itoa(c.Words))
+	}
+
+	if opts.ShouldShowBytes() {
+		stats = append(stats, strconv.Itoa(c.Bytes))
+	}
+
+	line := strings.Join(stats, "\t") + "\t"
+
+	fmt.Fprint(w, line)
+
+	suffixStr := strings.Join(suffixes, " ")
+	if suffixStr != "" {
+		fmt.Fprintf(w, " %s", suffixStr)
+	}
+
+	fmt.Fprint(w, "\n")
+}
+
+func GetCount(f io.Reader) Counts {
+	res := Counts{}
+
+	isInsideWord := false
+	reader := bufio.NewReader(f)
+
+	for {
+		r, size, err := reader.ReadRune()
+		if err != nil {
+			break
+		}
+
+		res.Bytes += size
+		if r == '\n' {
+			res.Lines++
+		}
+
+		isSpace := unicode.IsSpace(r)
+
+		if !isSpace && !isInsideWord {
+			res.Words++
+		}
+
+		isInsideWord = !isSpace
+	}
+
+	return res
+}
+
+func CountFile(filename string) (Counts, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return Counts{}, err
+	}
+	defer file.Close()
+
+	counts := GetCount(file)
+
+	return counts, nil
+}
+
+func CountWords(r io.Reader) int {
+	wordCount := 0
+
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		wordCount++
+	}
+
+	return wordCount
+}
+
+func CountLines(r io.Reader) int {
+	lineCount := 0
+
+	reader := bufio.NewReader(r)
+
+	for {
+		r, _, err := reader.ReadRune()
+		if err != nil {
+			break
+		}
+
+		if r == '\n' {
+			lineCount++
+		}
+	}
+
+	return lineCount
+}
+
+func CountBytes(r io.Reader) int {
+	bytesCount, _ := io.Copy(io.Discard, r)
+
+	return int(bytesCount)
+}
---
+++ counter/count_test.go
@@ -0,0 +1,371 @@
+package counter_test
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/henryppercy/counter/counter"
+	"github.com/henryppercy/counter/display"
+)
+
+func TestCountWords(t *testing.T) {
+	testCases := []struct {
+		name  string
+		input string
+		wants int
+	}{
+		{
+			name:  "5 words",
+			input: "one two three four five",
+			wants: 5,
+		},
+		{
+			name:  "empty input",
+			input: "",
+			wants: 0,
+		},
+		{
+			name:  "single space",
+			input: " ",
+			wants: 0,
+		},
+		{
+			name:  "new lines",
+			input: "one two three\nfour five",
+			wants: 5,
+		},
+		{
+			name:  "multiple spaces",
+			input: "This is a sentence.  This is another.",
+			wants: 7,
+		},
+		{
+			name:  "prefixed multiple spaces",
+			input: "   Hello",
+			wants: 1,
+		},
+		{
+			name:  "suffixed multiple spaces",
+			input: "Hello   ",
+			wants: 1,
+		},
+		{
+			name:  "tabbed character",
+			input: "Hello\tWorld\n",
+			wants: 2,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			r := strings.NewReader(tc.input)
+			gives := counter.GetCount(r).Words
+
+			if gives != tc.wants {
+				t.Logf("expected: %d got: %d", tc.wants, gives)
+				t.Fail()
+			}
+		})
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	testCases := []struct {
+		name  string
+		input string
+		wants int
+	}{
+		{
+			name:  "simple 5 words, 1 new line",
+			input: "one two three four five\n",
+			wants: 1,
+		},
+		{
+			name:  "empty file",
+			input: "",
+			wants: 0,
+		},
+		{
+			name:  "no new lines",
+			input: "one two three four five",
+			wants: 0,
+		},
+		{
+			name:  "no new lines at end",
+			input: "one two three four five\nsix",
+			wants: 1,
+		},
+		{
+			name:  "multi newline input",
+			input: "\n\n\n\n",
+			wants: 4,
+		},
+		{
+			name:  "multi word line string",
+			input: "one\ntwo\nthree\nfour\nfive\n",
+			wants: 5,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			r := strings.NewReader(tc.input)
+			gives := counter.GetCount(r).Lines
+
+			if gives != tc.wants {
+				t.Logf("expected: %d got: %d", tc.wants, gives)
+				t.Fail()
+			}
+		})
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	testCases := []struct {
+		name  string
+		input string
+		wants int
+	}{
+		{
+			name:  "five words",
+			input: "one two three four five",
+			wants: 23,
+		},
+		{
+			name:  "empty file",
+			input: "",
+			wants: 0,
+		},
+		{
+			name:  "all spaces",
+			input: "       ",
+			wants: 7,
+		},
+		{
+			name:  "newlines, tabs, and words",
+			input: "one\ntwo\nthree\nfour\t\n",
+			wants: 20,
+		},
+		{
+			name:  "Unicode characters",
+			input: "Mañana",
+			wants: 7,
+		},
+		{
+			name:  "",
+			input: "",
+			wants: 0,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			r := strings.NewReader(tc.input)
+			gives := counter.GetCount(r).Bytes
+
+			if gives != tc.wants {
+				t.Logf("expected: %d got: %d", tc.wants, gives)
+				t.Fail()
+			}
+		})
+	}
+}
+
+func TestGetCounts(t *testing.T) {
+	testCases := []struct {
+		name  string
+		input string
+		wants counter.Counts
+	}{
+		{
+			name:  "simple five words",
+			input: "one two three four five\n",
+			wants: counter.Counts{
+				Lines: 1,
+				Words: 5,
+				Bytes: 24,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			r := strings.NewReader(tc.input)
+			gives := counter.GetCount(r)
+
+			if gives != tc.wants {
+				t.Logf("expected: %d got: %d", tc.wants, gives)
+				t.Fail()
+			}
+		})
+	}
+}
+
+func TestPrintCount(t *testing.T) {
+	type inputs struct {
+		counts   counter.Counts
+		opts     display.Options
+		filename []string
+	}
+	testCases := []struct {
+		name  string
+		input inputs
+		wants string
+	}{
+		{
+			name: "simple five words",
+			input: inputs{
+				counts: counter.Counts{
+					Lines: 1,
+					Words: 5,
+					Bytes: 24,
+				},
+				opts: display.Options{
+					ShowBytes: true,
+					ShowLines: true,
+					ShowWords: true,
+				},
+				filename: []string{"words.txt"},
+			},
+			wants: "1\t5\t24\t words.txt\n",
+		},
+		{
+			name: "no file name",
+			input: inputs{
+				counts: counter.Counts{
+					Lines: 1,
+					Words: 4,
+					Bytes: 20,
+				},
+				opts: display.Options{
+					ShowBytes: true,
+					ShowWords: true,
+					ShowLines: true,
+				},
+			},
+			wants: "1\t4\t20\t\n",
+		},
+		{
+			name: "simple five words no options",
+			input: inputs{
+				counts: counter.Counts{
+					Lines: 1,
+					Words: 5,
+					Bytes: 24,
+				},
+				filename: []string{"words.txt"},
+			},
+			wants: "1\t5\t24\t words.txt\n",
+		},
+		{
+			name: "simple five words show lines",
+			input: inputs{
+				counts: counter.Counts{
+					Lines: 1,
+					Words: 5,
+					Bytes: 24,
+				},
+				opts: display.Options{
+					ShowBytes: false,
+					ShowWords: false,
+					ShowLines: true,
+				},
+				filename: []string{"words.txt"},
+			},
+			wants: "1\t words.txt\n",
+		},
+		{
+			name: "simple five words show words",
+			input: inputs{
+				counts: counter.Counts{
+					Lines: 1,
+					Words: 5,
+					Bytes: 24,
+				},
+				opts: display.Options{
+					ShowBytes: false,
+					ShowWords: true,
+					ShowLines: false,
+				},
+				filename: []string{"words.txt"},
+			},
+			wants: "5\t words.txt\n",
+		},
+		{
+			name: "simple five words show bytes and lines",
+			input: inputs{
+				counts: counter.Counts{
+					Lines: 1,
+					Words: 5,
+					Bytes: 24,
+				},
+				opts: display.Options{
+					ShowBytes: true,
+					ShowWords: false,
+					ShowLines: true,
+				},
+				filename: []string{"words.txt"},
+			},
+			wants: "1\t24\t words.txt\n",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			buffer := &bytes.Buffer{}
+
+			tc.input.counts.Print(buffer, tc.input.opts, tc.input.filename...)
+
+			if buffer.String() != tc.wants {
+				t.Logf("expected: %v got: %v", tc.wants, buffer.String())
+				t.Fail()
+			}
+		})
+	}
+}
+
+func TestAddCount(t *testing.T) {
+	type inputs struct {
+		counts counter.Counts
+		other  counter.Counts
+	}
+	testCases := []struct {
+		name  string
+		input inputs
+		wants counter.Counts
+	}{
+		{
+			name: "simple add by one",
+			input: inputs{
+				counts: counter.Counts{
+					Lines: 1,
+					Words: 5,
+					Bytes: 24,
+				},
+				other: counter.Counts{
+					Lines: 1,
+					Words: 1,
+					Bytes: 1,
+				},
+			},
+			wants: counter.Counts{
+				Lines: 2,
+				Words: 6,
+				Bytes: 25,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			totals := tc.input.counts
+			res := totals.Add(tc.input.other)
+
+			if res != tc.wants {
+				t.Logf("expected: %v got: %v", tc.wants, res)
+				t.Fail()
+			}
+		})
+	}
+}
