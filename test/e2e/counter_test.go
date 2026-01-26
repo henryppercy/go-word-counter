7b2317f7bed4434333b36a8d878cd0f48fab5cff
henryppercy <henryppercy@gmail.com>
2026-01-26 16:54:05 +0000

    feat: add file handling tests

--- test/e2e/counter_test.go
+++ test/e2e/counter_test.go
@@ -2,6 +2,7 @@ package e2e
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -9,15 +10,24 @@ import (
 	"testing"
 )
 
-func TestStdin(t *testing.T) {
+func getCommand(args ...string) (*exec.Cmd, error) {
 	dir, err := os.Getwd()
 	if err != nil {
-		t.Fatal("couldn't get pwd: ", err)
+		return nil, err
 	}
-
+	
 	path := filepath.Join(dir, binName)
+	
+	cmd := exec.Command(path, args...)
 
-	cmd := exec.Command(path)
+	return cmd, nil;
+}
+
+func TestStdin(t *testing.T) {
+	cmd, err :=  getCommand()
+	if err != nil {
+		t.Fatal("failed to get pwd: ", err)
+	}
 
 	output := &bytes.Buffer{}
 
@@ -31,5 +41,79 @@ func TestStdin(t *testing.T) {
 	wants := " 1 3 14\n"
 	if wants != output.String() {
 		t.Log("stdout is not correct: wanted: ", wants, "got: ", output.String())
+	}
+}
+
+func TestSingleFile(t *testing.T) {
+	file, err := os.CreateTemp("", "counter-test-*")
+	if err != nil {
+		t.Fatal("failed to create temp file:", err)
+	}
+
+	defer os.Remove(file.Name())
+
+	_, err = file.WriteString("foo bar baz\nbaz bar foo\none two three\n")
+	if err != nil {
+		t.Fatal("failed to write to temp file", err)
+	}
+
+	err = file.Close()
+	if err != nil {
+		t.Fatal("failed to close file:", err)
+	}
+
+	cmd, err := getCommand(file.Name())
+	if err != nil {
+		t.Fatal("failed to get pwd: ", err)
+	}
+
+	output := &bytes.Buffer{}
+	cmd.Stdout = output
+
+	if err = cmd.Run(); err != nil {
+		t.Fatal("failed to run command", err)
+	}
+
+	wants := fmt.Sprintf(" 3 9 38 %s\n", file.Name())
+	if output.String() != wants {
+		t.Log("stdout is not correct: wanted: ", wants, "got: ", output.String())
+		t.Fail()
+	}
+}
+
+func TestNoExist(t *testing.T) {
+	cmd, err := getCommand("no_exist.txt")
+	if err != nil {
+		t.Fatal("failed to get pwd: ", err)
+	}
+
+	stderr := &bytes.Buffer{}
+	stdout := &bytes.Buffer{}
+
+	cmd.Stderr = stderr
+	cmd.Stdout = stdout
+
+	wantsStderr := "counter: open no_exist.txt: no such file or directory\n"
+	wantsStdout := ""
+
+	if err = cmd.Run()
+	err == nil {
+		t.Log("command succeeded when should have failed")
+		t.Fail()
+	}
+
+	if err.Error() != "exit status 1" {
+		t.Log("expected error of exit status 1. got:", err.Error())
+		t.Fail()
+	}
+
+	if stderr.String() != wantsStderr {
+		t.Log("stderr is not correct: wanted: ", wantsStderr, "got: ", stderr.String())
+		t.Fail()
+	}
+
+	if stdout.String() != wantsStdout {
+		t.Log("stdout is not correct: wanted: ", wantsStdout, "got: ", stdout.String())
+		t.Fail()
 	}
 }
